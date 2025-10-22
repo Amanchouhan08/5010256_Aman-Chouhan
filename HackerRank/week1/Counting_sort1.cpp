d1388469d9ecb455c326ea17b7a0aa2e13632426
Aman Chouhan <ak6927887@gmail.com>
2025-10-22 21:53:29 +0530

    Delete HackerRank/week1 directory

--- HackerRank/week1/Counting_sort1.cpp
+++
@@ -1,7 +0,0 @@
-vector<int> countingSort(vector<int> arr) {
-     vector<int> ans(100,0);
-    for(int i=0; i<arr.size(); i++){
-        ans[arr[i]]++;
-    }
-    return ans;
-}
--- HackerRank/week1/Flipping_bits.cpp
+++
@@ -1,6 +0,0 @@
-long flippingBits(long n) {
-      for(int i=0; i<32; i++){
-        n^=1u<<i;
-    }
-    return n;
-}
